572ed2876b9fa95ce94bc8687684c622ad8887f1
GraceDouX <126838063+GraceDouX@users.noreply.github.com>
2023-03-03 10:03:19 +0800

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,11 @@
+money = 50
+print("钱包还有:", money)
+
+# 买了一个冰淇淋减去10元
+money = money - 10
+print("余额:", money, "元")
+
+# 假设, 每隔一小时输出一下钱包余额
+print("现在是下午2点, 钱包余额剩余:", money)
+print("现在是下午3点, 钱包余额剩余:", money)
+print("现在是下午4点, 钱包余额剩余:", money)
